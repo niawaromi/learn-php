c85017ad53c3f14ae0c0574038d60747cffa98ad
Nia Waromi <91767702+niawaromi@users.noreply.github.com>
2022-03-26 08:43:56 +0700

    write comments in php

---
+++ index2.php
@@ -0,0 +1,15 @@
+<meta charset="UTF-8">
+<title>write comments in PHP</title>
+</head>
+
+<body>
+    <?php 
+    //This gets something from the database
+    function () {
+        
+    }
+   /* echo "Hi";
+    echo "Junior"*/
+    ?>
+</body>
+</html>
