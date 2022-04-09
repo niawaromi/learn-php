58fbf8c4e942cbbfb2e5710e268b1eb33b7eb329
Nia Waromi <91767702+niawaromi@users.noreply.github.com>
2022-04-09 08:30:21 +0700

    .

---
+++ arithmetic-operators-in-php/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Arothmetic Operators</title>
+    </head>
+
+    <body>
+    <?php 
+
+    //Arithmetic Operators
+    echo 5;
+    
+    ?>    
+    </body>
+</html>
---
+++ type-in-php/index.php
@@ -0,0 +1,24 @@
+<html>
+    
+<body>
+<?php 
+   //string
+   $name = 'Coding is fun'; 
+
+   //Integer
+   $name = 20;
+
+   //Float
+   $name = 20.5784;
+
+   //Boolean
+   true = 1
+   false= 0
+
+   //Array
+   $names = array("Daniel","Denis","JU");
+   echo $names ['0'];
+?>
+    
+</body>
+</html>
