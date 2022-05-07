62dab6e13883ba3455bbf9311cb210d1142cd4bd
Nia Waromi <91767702+niawaromi@users.noreply.github.com>
2022-05-07 09:34:09 +0700

    .

---
+++ insert-data-from-website-into-database-using-mysqli/index.php
@@ -0,0 +1,24 @@
+<?php 
+    include_once 'includes/dbh.inc.php';
+?>
+
+<DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            
+        <?php
+           $sql = "SELECT * FROM users;";
+           $result = mysqli_query($conn, $sql);
+           $resultCheck = mysqli_fetch_rows($result);
+
+           if ($resultCheck > 0) {
+               while ($row = mysqli_fetch_assoc($result)) {
+                   echo $row['user_uid'] . "<br>";
+               }
+           }
+           ?>
+        </body>
+    </html>
