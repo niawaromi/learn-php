d35b45e66ee094ac189d55ae21cc5cb3ad064254
Nia Waromi <91767702+niawaromi@users.noreply.github.com>
2022-05-14 09:33:00 +0700

    .

---
+++ connect-database-in-php/index.php
@@ -0,0 +1,14 @@
+<php? 
+    include_once 'includes/dbh.inc.php';
+
+?>
+
+<DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            $conn;
+        </body>
+    </html>
